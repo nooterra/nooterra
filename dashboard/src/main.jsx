79fd18d40b73a603e0cf309ace999f05d0f4adec
Aiden <aidenlippert@gmail.com>
2026-02-21 21:14:50 -0800

    Remove legacy marketing pages and keep single-page website

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,25 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 import App from "./App.jsx";
-import { auth0AuthorizationParams, auth0ClientId, auth0Domain, auth0Enabled } from "./site/auth/auth0-config.js";
 import "./styles/globals.css";
 
-const appTree = (
-  <React.StrictMode>
-    {auth0Enabled ? (
-      <Auth0Provider
-        domain={auth0Domain}
-        clientId={auth0ClientId}
-        authorizationParams={auth0AuthorizationParams()}
-      >
-        <App />
-      </Auth0Provider>
-    ) : (
-      <App />
-    )}
-  </React.StrictMode>
-);
+const appTree = <React.StrictMode><App /></React.StrictMode>;
 
 ReactDOM.createRoot(document.getElementById("root")).render(appTree);
